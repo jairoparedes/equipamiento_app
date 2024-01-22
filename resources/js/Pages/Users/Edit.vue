1fdf0140334a7eb6acbf454b44ee2426ff3a1ec9
jairoparedes <hiro.paredes.candia@gmail.com>
2024-01-22 14:47:04 -0300

    Se modifican las vistas
    Se modifican las vistas de las paginas de equipos, equipo_user y user

---
+++ resources/js/Pages/Users/Edit.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <!-- Contenido del componente -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // Opciones y lógica del componente
+  };
+  </script>
+  
+  <style>
+  /* Estilos del componente */
+  </style>
+  
